03196247dfa80e3320fd56fa244a5f125ade67a1
Isabella M <isabella.minergard@outlook.com>
2026-05-11 14:21:43 +0200

    updates

--- camera_vision/depthai_test/src/main.cpp
+++ camera_vision/depthai_test/src/main.cpp
@@ -1,32 +1,23 @@
 #include <iostream>
-#include <memory>
-#include <opencv2/opencv.hpp>
-
+#include "opencv2/opencv.hpp"
 #include "depthai/depthai.hpp"
 
+
 int main() {
-    // Create device
-    std::shared_ptr<dai::Device> device = std::make_shared<dai::Device>();
+    dai::Pipeline pipeline;
 
-    // Create pipeline
-    dai::Pipeline pipeline(device);
+    auto cam = pipeline.create<dai::node::Camera>();
+    auto xout = pipeline.create<dai::node::XLinkOut>();
 
-    // Create nodes
-    auto cam = pipeline.create<dai::node::Camera>()->build();
-    auto videoQueue = cam->requestOutput(std::make_pair(640, 400))->createOutputQueue();
+    xout->setStreamName("video");
 
-    // Start pipeline
-    pipeline.start();
+    cam->video.link(xout->input);
 
-    while(true) {
-        auto videoIn = videoQueue->get<dai::ImgFrame>();
-        if(videoIn == nullptr) continue;
-
-        cv::imshow("video", videoIn->getCvFrame());
-
-        if(cv::waitKey(1) == 'q') {
-            break;
-        }
+    try {
+        dai::Device device(pipeline);
+    } catch(const std::exception& e) {
+        std::cerr << "No device found: " << e.what() << std::endl;
+        return 1;
     }
 
     return 0;
